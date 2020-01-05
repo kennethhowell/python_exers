ba4a80dccc0917060a8f503487619c060541663a
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-01-05 16:03:16 -0600

    working through exers

--- py_one.py
+++ py_one.py
@@ -1,5 +1,15 @@
 # print("Hello, Daddy! Hello, Mom.")
 
-userInput = input("Hello to who?")
+# userInput = input("Hello to who?")
+#
+# print(userInput)
 
-print(userInput)
+x = "awesome"
+
+def myAwesomeFunction():
+    x = "fantastic"
+    print("Python is " + x)
+
+myAwesomeFunction()
+
+print("Python is " + x)
